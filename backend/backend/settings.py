cf1d31999c5cfa436c8b15b7c150056299ad3f0a
Stefan <scatiru1231@gmail.com>
2024-11-12 11:50:06 +0200

    feat: add secret key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,16 +16,10 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d9!99x@hufv3oyo=&0$pr7k+vuv^btp@)z@g2(j2p_41o&i7s8'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
