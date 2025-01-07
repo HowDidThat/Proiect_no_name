ca9efdb14dd9f851db9f01ab0a90da068ecce95d
Briana-Stefania Maftei <bsmaftei@amazon.com>
2025-01-07 22:11:12 +0200

    Refactor database configuration to use environment variables

--- ml/ml/settings.py
+++ ml/ml/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'ml.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'symptom_db',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
